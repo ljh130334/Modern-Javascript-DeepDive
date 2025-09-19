26d62053f8a1b3f13d567d5fc4608f6676792a0d
fr0gydev <ljh130334@naver.com>
2025-09-19 10:45:20 +0900

    Feat: 예제 03-03

--- 3장/3.4/index.js
+++ 3장/3.4/index.js
@@ -1,3 +1,7 @@
 const arr = [1, 2, 3];
 
-arr.forEach(console.log);
+arr.forEach(alert);
+
+// alert 함수는 브라우저에서만 동작하는 클라이언트 사이드 Web API입니다.
+// Node.js 환경에서는 alert 함수를 사용할 수 없습니다.
+// 클라이언트 사이드 Web API가 포함된 소스코드는 Code Runner 확장 프로그램에서 실행할 수 없습니다.
